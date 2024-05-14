191f5b21a207090439d4bddd939fff8ad15f4874
VasilevAlexandr97 <vasilev.alex.work@gmail.com>
2024-05-14 19:52:17 +0500

    add transaction schema

--- bitpapa_pay/schemas/addresses.py
+++ bitpapa_pay/schemas/addresses.py
@@ -1,7 +1,7 @@
 from typing import List, Optional, Union
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Address(BaseModel):
@@ -32,5 +32,18 @@ class CreateAddressOutputData(BaseModel):
     address: Address
 
 
+class Transaction(BaseModel):
+    id: UUID
+    direction: str
+    txhash: Optional[str]
+    currency: str
+    network: Optional[str]
+    amount: Union[str, float]
+    from_: Optional[str] = Field(alias="from")
+    to: Optional[str]
+    input: Optional[str]
+    label: Optional[str]
+
+
 class GetTransactionsOutputData(BaseModel):
-    transaction: List
+    transaction: List[Transaction]
